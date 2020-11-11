2da1000c38411dac28a540353ef561647cbea6df
Andres <Andres@DESKTOP-3LVNFEG>
2020-11-10 23:39:25 -0500

    creacion de clases

--- src/ec/edu/ups/modelo/Persona.java
+++ src/ec/edu/ups/modelo/Persona.java
@@ -5,10 +5,93 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.Objects;
+
 /**
  *
  * @author Andres
  */
 public class Persona {
-    
+   private int id;
+   private String cedula;
+   private String nombre;
+   private String apellido;
+
+    public Persona() {
+    }
+
+    public Persona(int id, String cedula, String nombre, String apellido) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.cedula);
+        hash = 71 * hash + Objects.hashCode(this.apellido);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.cedula, other.cedula)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellido, other.apellido)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+   
+   
 }
---
+++ src/ec/edu/ups/modelo/Telefono.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Andres
+ */
+public class Telefono {
+  private String operadora;
+  private String numero;
+  private TipoTelefono tipo;
+
+    public Telefono() {
+    }
+
+    public Telefono(String operadora, String numero, TipoTelefono tipo) {
+        this.operadora = operadora;
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+
+    public String getOperadora() {
+        return operadora;
+    }
+
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public TipoTelefono getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoTelefono tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + Objects.hashCode(this.numero);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefono other = (Telefono) obj;
+        if (!Objects.equals(this.numero, other.numero)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefono{" + "operadora=" + operadora + ", numero=" + numero + ", tipo=" + tipo + '}';
+    }
+  
+  
+}
